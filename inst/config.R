8cf8b7fadbb19325368f6306bd211655b8a3c01b
ces0491 <cesairetobias@hotmail.com>
2020-11-09 15:28:51 +0200

    Initial commit

---
+++ inst/config.R
@@ -0,0 +1,8 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Cesaire Tobias",
+  data_location = "./extdata",
+  pdf_location = "https://github.com/ces0491/cesCV/raw/master/ctobias_cv.pdf",
+  html_location = "https://ces0491.github.io/ctobias_cv",
+  source_location = "https://github.com/ces0491/cv",
+  output_dir = here::here()
+)
